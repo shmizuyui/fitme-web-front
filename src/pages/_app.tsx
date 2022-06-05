4cb87845ae5d9748b6f5000611bfe72ec4fd0210
shimizzuu <yuta6113@gmail.com>
2022-06-05 10:36:01 +0900

    環境構築

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import Head from "next/head";
+import { GlobalNav } from "../components/common/GlobalNav";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Fitme</title>
+        <link rel="app icon" href="/sample.jpg" />
+      </Head>
+      <GlobalNav />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;

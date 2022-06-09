51095e6fb52620bd7ea769fa4f6b4df2ae8748da
shimizzuu <yuta6113@gmail.com>
2022-06-09 12:22:53 +0900

    テスト追加

---
+++ src/components/common/GlobalNav/index.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { GlobalNav } from ".";
+
+describe("GlobalNav", () => {
+  test("コンポーネントをレンダリングすること", () => {
+    render(<GlobalNav />);
+
+    expect(screen.getByRole("img")).toHaveAttribute("src", "/fitme.svg");
+
+    const { container } = render(<GlobalNav />);
+    const buttons = container.getElementsByTagName("button");
+    const lists = container.getElementsByTagName("li");
+
+    expect(buttons.length).toBe(2);
+    expect(buttons[0].textContent).toBe("新規登録");
+    expect(buttons[1].textContent).toBe("ログイン");
+
+    expect(lists.length).toBe(4);
+    expect(lists[0].textContent).toBe("Home");
+    expect(lists[1].textContent).toBe("Fitmeとは");
+    expect(lists[2].textContent).toBe("トレーナー一覧");
+    expect(lists[3].textContent).toBe("レッスン一覧");
+  });
+});

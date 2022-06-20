1d51aaebfee465a93e72c6ee10ed4a30030d8faa
shmizuyui <yuihana760@gmail.com>
2022-06-20 16:08:53 +0900

    ページネーション機能追加

--- src/hooks/user/useLessons.ts
+++ src/hooks/user/useLessons.ts
@@ -1,14 +1,22 @@
-import useSWR from "swr";
-import { fetcher } from "../../apis/client";
-import { Lesson } from "../../apis/models/lesson";
-import { BaseType } from "../type";
+import useSWR from 'swr';
+import {fetcher} from '../../apis/client';
+import {Lesson} from '../../apis/models/lesson';
+import {Pagination} from '../../apis/models/pagination';
+import {BaseType} from '../type';
 
-export const useLessons = () => {
-  const { data, error } = useSWR(`/api/v1/user/lessons`, fetcher);
+type LessonsResponse = {
+  lessons: Lesson[];
+} & Pagination;
+
+export const useLessons = (pageIndex: number) => {
+  const {data, error} = useSWR(
+      `/api/v1/user/lessons?page=${pageIndex}`,
+      fetcher,
+  );
   return {
-    data: data?.data?.lessons,
+    data: data?.data,
     error: data?.error,
     isLoading: !error && !data,
     isError: error,
-  } as BaseType<Lesson[]>;
+  } as BaseType<LessonsResponse>;
 };
